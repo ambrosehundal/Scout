47c22b91eed969915ab8bbdd8b6055c465cbc212
Ambrose Hundal <amshunda@ucsc.edu>
2019-03-02 14:47:16 -0800

    add devise to each athlete model instance

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :athletes
 
   root 'athletes#index'
+  devise_for :athletes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
