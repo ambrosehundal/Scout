8f3e1352f4f08fd8b4b760e3d0211782c9d84cee
Ambrose Hundal <amshunda@ucsc.edu>
2019-03-05 22:58:29 -0800

    message conversation

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,3 +1,7 @@
 class Athlete < ApplicationRecord
     has_many :awards
+
+    has_many :messages, class_name: "Private::Message", foreign_key: :conversation_id
+
+    belongs_to :sender, foreign_key: :sender_id, class_name: ''
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :athletes
 
   root 'athletes#index'
-  devise_for :athletes
+  #devise_for :athletes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
